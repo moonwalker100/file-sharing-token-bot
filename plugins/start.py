b6dc963fa684ea7b1321e8689d5c1fbcedc9393a
Moonwalker <moonwalker1092@gmail.com>
2025-11-05 21:25:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -27,6 +27,7 @@ from config import (
     PROTECT_CONTENT,
     TUT_VID,
     OWNER_ID,
+    PICS,
 )
 from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
 from database.database import add_user, del_user, full_userbase, present_user
